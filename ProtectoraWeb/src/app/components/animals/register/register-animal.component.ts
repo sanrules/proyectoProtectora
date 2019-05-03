eaef02db42f0d5c0cac17ae5322ea1b60dac4a4d
Oscar Hernandez <oscarhernandezfernandez16@gmail.com>
2019-05-03 13:36:23 +0200

    Formulario registro animal con validaciones basicas

--- ProtectoraWeb/src/app/components/animals/register/register-animal.component.ts
+++ ProtectoraWeb/src/app/components/animals/register/register-animal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registro-animal',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterAnimalComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name: ['Nombre', [Validators.required]],
+      type: ['Tipo', [Validators.required]],
+      breed: ['Raza', [Validators.required]],
+      gender: ['Genero', [Validators.required]],
+      birthDate: ['Fecha de Nacimiento', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      entranceDate: ['Fecha de Entrada', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      adoptionDate: ['', []],
+      status: ['sin adoptar', []],
+      description: ['Descripcion', [Validators.required]],
+      pictures: ['', []]
+
+    });
+  }
+  dataParse() {}
+
+  registerSubmit() {
+    console.log(this.registerForm.value);
   }
 
 }
