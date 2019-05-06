a4e4821b73abfbba60989b1cd560f1a761cb8994
Sandra Cerro <sandra@inserver.es>
2019-05-06 15:16:16 +0200

    validaciones user

--- ProtectoraWebApi/controller/user/get_all_users.php
+++ ProtectoraWebApi/controller/user/get_all_users.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- * Obtiene todos los useres de la base de datos, da formato y valida el mismo y los devuelve
+ * Obtiene todos los usuarios de la base de datos, da formato y valida el mismo y los devuelve
  * @return array $users array de user obtenido de la bbdd
  */
 
-    require_once '../../lib/connection.php';
-    require_once '../../model/user_model.php';
+require_once '../../lib/connection.php';
+require_once '../../model/user_model.php';
 
-    ChromePhp::log('get_user_all()');
+ChromePhp::log('get_user_all()');
 
-    $users = retrieve_user_all();
-    ChromePhp::log($users);
+$users = retrieve_user_all();
+ChromePhp::log($users);
 
-    echo json_encode($users);
-    header('Content-Type: application/json');
+echo json_encode($users);
+header('Content-Type: application/json');
--- ProtectoraWebApi/controller/user/insert_user.php
+++ ProtectoraWebApi/controller/user/insert_user.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once '../../lib/connection.php';
+// header("Access-Control-Allow-Origin");
+// header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+// header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+// header('Content-type: application/json');
 
 ChromePhp::log('Entra en insert_user');
 
@@ -10,26 +14,39 @@ ChromePhp::log('insert_user $postdata: ', $postdata);
 ChromePhp::log('insert_user $request: ', $request);
 
 if ($request) {
-    //$data = $_REQUEST['createuser'];
-    //$dates      = explode(',', $data);
-    //$new  = json_decode($data);
-    $user = R::dispense('user');
+    // Validamos los datos y los filtramos
+    $username   = filter_var($request->userName, FILTER_SANITIZE_STRING);
+    $email      = filter_var($request->email, FILTER_VALIDATE_EMAIL) ? $request['email'] : '';
+    $password   = filter_var($request->password, FILTER_SANITIZE_STRING);
+    $name       = filter_var($request->name, FILTER_SANITIZE_STRING);
+    $surname    = filter_var($request->surname, FILTER_SANITIZE_STRING);
+    $phone      = filter_var($request->phone, FILTER_SANITIZE_NUMBER_INT);
+    $birth_date = validate_date($request->birthDate) ? $request->data->birth_date : '';
+    $street     = filter_var($request->street, FILTER_SANITIZE_STRING);
+    $number     = filter_var($request->number, FILTER_SANITIZE_NUMBER_INT);
+    $user_type  = filter_var($request->userType, FILTER_SANITIZE_STRING);
 
-    $user->username   = $request->userName;
-    $user->password   = $request->password;
-    $user->name       = $request->name;
-    $user->surname    = $request->surname;
-    $user->email      = $request->email;
-    $user->phone      = $request->phone;
-    $user->birth_date = $request->birthDate;
-    $user->street     = $request->street;
-    $user->number     = $request->number;
-    $user->floor     = $request->floor;
-    $user->door     = $request->door;
-    $user->user_type  = $request->userType;
+    // Comprobamos que todo viene con datos. Si no, se devolverá al formulario
+    if ($username != '' || $email != '' || $password != '' || $name != '' || $surname != '' || $phone != '' || $birth_date != '' || $street != '' || $user_type != '') {
+        $user->username   = $username;
+        $user->email      = $email;
+        $user->password   = $password;
+        $user->name       = $name;
+        $user->surname    = $surname;
+        $user->phone      = $phone;
+        $user->birth_date = $birth_date;
+        $user->street     = $street;
+        $user->number     = $number;
+        $user->floor      = $request->floor;
+        $user->door       = $request->door;
+        $user->user_type  = $user_type;
 
-    $id = R::store($user);
-    ChromePhp::log('insert_user $user: ', $user);
-    ChromePhp::log('insert_user $id: ', $id);
-    ChromePhp::log('sale de insert_user');
+        $user = R::dispense('user');
+
+        $id = R::store($user);
+        ChromePhp::log('insert_user $user: ', $user);
+        ChromePhp::log('insert_user $id: ', $id);
+        ChromePhp::log('sale de insert_user');
+    }
+
 }
