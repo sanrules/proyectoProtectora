989c04c13fd32ffd287b00641055f088d7601e44
sanrules <sankiry93@gmail.com>
2019-05-16 20:45:53 +0200

    insertUser y clase user

--- ProtectoraWebApi/class/user.php
+++ ProtectoraWebApi/class/user.php
@@ -2,36 +2,344 @@
 require '../lib/RedBeanPHP5_3/rb.php';
 class User
 {
-    public $id_user    = ''; //  idUser
-    public $username   = ''; //  userName
-    public $email      = ''; //  email
-    public $password   = ''; //  password
-    public $name       = ''; // name
-    public $surname    = ''; // surname
-    public $phone      = ''; // phone
-    public $birth_date = ''; // birthDate
-    public $street     = ''; // street
-    public $number     = ''; // number
-    public $portal     = ''; // portal
-    public $floor      = ''; // floor
-    public $door       = ''; // door
-    public $user_type  = ''; // userType
+    private $_idUser    = ''; //  idUser
+    private $_username  = ''; //  userName
+    private $_email     = ''; //  email
+    private $_password  = ''; //  password
+    private $_name      = ''; // name
+    private $_surname   = ''; // surname
+    private $_phone     = ''; // phone
+    private $_birthDate = ''; // birthDate
+    private $_street    = ''; // street
+    private $_number    = ''; // number
+    private $_portal    = ''; // portal
+    private $_floor     = ''; // floor
+    private $_door      = ''; // door
+    private $_userType  = ''; // userType
 
-    // public function __construct($name, $type, $breed, $gender, $birth_date, $entrance_date, $adoption_date = null, $status, $description, $pictures)
-    // {
-    //     $this->$name = $name;
-    //     $this->$type = $type;
-    //     $this->$breed = $breed;
-    //     $this->$gender = $gender;
-    //     $this->$birth_date = $birth_date;
-    //     $this->$entrance_date = $entrance_date;
-    //     $this->$adoption_date = $adoption_date;
-    //     $this->$status = $status;
-    //     $this->$description = $description;
-    //     $this->$pictures = $pictures;
-    // }
+    private function __construct($_idUser, $_username, $_email, $_password, $_name, $_surname, $_phone, $_birthDate, $_street, $_number, $_portal, $_floor, $_door, $_userType)
+    {
+        $this->_username  = $_username;
+        $this->_email     = $_email;
+        $this->_name      = $_name;
+        $this->_surname   = $_surname;
+        $this->_phone     = $_phone;
+        $this->_birthDate = $_birthDate;
+        $this->_street    = $_street;
+        $this->_number    = $_number;
+        $this->_portal    = $_portal;
+        $this->_door      = $_door;
+        $this->_userType  = $_userType;
+    }
 
-    // public function __destruct()
-    // {}
+    /**
+     * Inserta un usuario en la bbdd
+     */
+    public function insertUser()
+    {
+        $bbddUser = R::dispense('user');
 
+        $bbddUser->username  = $this->_username;
+        $bbddUser->email     = $this->_email;
+        $bbddUser->name      = $this->_name;
+        $bbddUser->surname   = $this->_surname;
+        $bbddUser->phone     = $this->_phone;
+        $bbddUser->birthDate = $this->_birthDate;
+        $bbddUser->street    = $this->_street;
+        $bbddUser->number    = $this->_number;
+        $bbddUser->portal    = $this->_portal;
+        $bbddUser->door      = $this->_door;
+        $bbddUser->userType  = $this->_userType;
+
+        $id = R::store($bbddUser);
+
+        $this->_id = $id;
+
+        // TODO ALMACENAR PASSWORD SEGURA
+
+    }
+
+    /*
+     *** GETTERS Y SETTERS
+     */
+
+    /**
+     * Get the value of _idUser
+     */
+    public function get_idUser()
+    {
+        return $this->_idUser;
+    }
+
+    /**
+     * Set the value of _idUser
+     *
+     * @return  self
+     */
+    public function set_idUser($_idUser)
+    {
+        $this->_idUser = $_idUser;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _username
+     */
+    public function get_username()
+    {
+        return $this->_username;
+    }
+
+    /**
+     * Set the value of _username
+     *
+     * @return  self
+     */
+    public function set_username($_username)
+    {
+        $this->_username = $_username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _email
+     */
+    public function get_email()
+    {
+        return $this->_email;
+    }
+
+    /**
+     * Set the value of _email
+     *
+     * @return  self
+     */
+    public function set_email($_email)
+    {
+        $this->_email = $_email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _password
+     */
+    public function get_password()
+    {
+        return $this->_password;
+    }
+
+    /**
+     * Set the value of _password
+     *
+     * @return  self
+     */
+    public function set_password($_password)
+    {
+        $this->_password = $_password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _name
+     */
+    public function get_name()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * Set the value of _name
+     *
+     * @return  self
+     */
+    public function set_name($_name)
+    {
+        $this->_name = $_name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _surname
+     */
+    public function get_surname()
+    {
+        return $this->_surname;
+    }
+
+    /**
+     * Set the value of _surname
+     *
+     * @return  self
+     */
+    public function set_surname($_surname)
+    {
+        $this->_surname = $_surname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _phone
+     */
+    public function get_phone()
+    {
+        return $this->_phone;
+    }
+
+    /**
+     * Set the value of _phone
+     *
+     * @return  self
+     */
+    public function set_phone($_phone)
+    {
+        $this->_phone = $_phone;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _birthDate
+     */
+    public function get_birthDate()
+    {
+        return $this->_birthDate;
+    }
+
+    /**
+     * Set the value of _birthDate
+     *
+     * @return  self
+     */
+    public function set_birthDate($_birthDate)
+    {
+        $this->_birthDate = $_birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _street
+     */
+    public function get_street()
+    {
+        return $this->_street;
+    }
+
+    /**
+     * Set the value of _street
+     *
+     * @return  self
+     */
+    public function set_street($_street)
+    {
+        $this->_street = $_street;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _number
+     */
+    public function get_number()
+    {
+        return $this->_number;
+    }
+
+    /**
+     * Set the value of _number
+     *
+     * @return  self
+     */
+    public function set_number($_number)
+    {
+        $this->_number = $_number;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _portal
+     */
+    public function get_portal()
+    {
+        return $this->_portal;
+    }
+
+    /**
+     * Set the value of _portal
+     *
+     * @return  self
+     */
+    public function set_portal($_portal)
+    {
+        $this->_portal = $_portal;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _floor
+     */
+    public function get_floor()
+    {
+        return $this->_floor;
+    }
+
+    /**
+     * Set the value of _floor
+     *
+     * @return  self
+     */
+    public function set_floor($_floor)
+    {
+        $this->_floor = $_floor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _door
+     */
+    public function get_door()
+    {
+        return $this->_door;
+    }
+
+    /**
+     * Set the value of _door
+     *
+     * @return  self
+     */
+    public function set_door($_door)
+    {
+        $this->_door = $_door;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _userType
+     */
+    public function get_userType()
+    {
+        return $this->_userType;
+    }
+
+    /**
+     * Set the value of _userType
+     *
+     * @return  self
+     */
+    public function set_userType($_userType)
+    {
+        $this->_userType = $_userType;
+
+        return $this;
+    }
 }
---
+++ ProtectoraWebApi/controller/insertUser.php
@@ -0,0 +1,44 @@
+<?php
+
+//header("Access-Control-Allow-Origin");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Content-type: application/json');
+
+require_once '../lib/RedBeanPHP5_3/rb.php';
+R::setup('mysql:host=localhost;dbname=proyecto',
+    'root', '');
+
+$error    = array();
+$response = '';
+
+if ($_REQUEST['createuser']) {
+    $jsonData = $_REQUEST['createuser'];
+    $userData = json_decode($data);
+
+    // Sanitize & validate
+    isset($userData->username) ? $username   = $userData->username : $error['username']   = 'not set';
+    isset($userData->email) ? $email         = $userData->email : $error['email']         = 'not set';
+    isset($userData->name) ? $name           = $userData->name : $error['name']           = 'not set';
+    isset($userData->surname) ? $surname     = $userData->surname : $error['surname']     = 'not set';
+    isset($userData->phone) ? $phone         = $userData->phone : $error['phone']         = 'not set';
+    isset($userData->birthDate) ? $birthDate = $userData->birthDate : $error['birthDate'] = 'not set';
+    isset($userData->street) ? $street       = $userData->street : $error['street']       = 'not set';
+    isset($userData->number) ? $number       = $userData->number : $error['number']       = 'not set';
+    isset($userData->portal) ? $portal       = $userData->portal : $error['portal']       = 'not set';
+    isset($userData->door) ? $door           = $userData->door : $error['door']           = 'not set';
+    isset($userData->userType) ? $userType   = $userData->userType : $error['userType']   = 'not set';
+
+    // Si no hay errores se crea el usuario
+    if (count($error) == 0) {
+        $user = new User($username, $email, $name, $surname, $phone, $birthDate, $street, $number, $portal, $door, $userType);
+
+        $user->insertUser();
+
+        $response = 'usuario insertado correctamente';
+    } else {
+        $response = $error;
+    }
+
+    echo json_encode($response, JSON_UNESCAPED_UNICODE);
+}
