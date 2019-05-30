8ace9c6dd1f0ad93d045d428ef23eb102bf27931
Fernando <fgironpe@gmal.com>
2019-05-30 17:53:19 +0200

    Arreglados pequeños fallos

--- ProtectoraWeb/src/app/_services/auth/auth.service.ts
+++ ProtectoraWeb/src/app/_services/auth/auth.service.ts
@@ -90,12 +90,11 @@ export class AuthService {
   */
   decodeJWT(token: string) {
     const base64Url = token.split('.')[1];
-    const base64 = decodeURIComponent(atob(base64Url).split('').map(function(c) {
+    const base64 = decodeURIComponent(atob(base64Url).split('').map(c => {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 
     return JSON.parse(base64);
   }
-
 
 }
