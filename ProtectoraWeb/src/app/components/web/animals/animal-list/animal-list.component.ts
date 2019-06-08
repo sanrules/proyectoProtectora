9fbec3cab7dfc889914832a056c88dd875806c88
Fernando <fgironpe@gmail.com>
2019-06-08 22:35:25 +0200

    Cambios en lista de animal

--- ProtectoraWeb/src/app/components/web/animals/animal-list/animal-list.component.ts
+++ ProtectoraWeb/src/app/components/web/animals/animal-list/animal-list.component.ts
@@ -1,5 +1,7 @@
 import { OnInit, Component } from '@angular/core';
 import { AnimalService } from '../../../../_services/animals/animal/animal-service';
+import { ImagesService } from '../../../../_services/animals/images/images.service';
+import { Animal } from 'src/app/_models/animal.model';
 
 @Component({
     selector: 'app-animal-list',
@@ -8,15 +10,24 @@ import { AnimalService } from '../../../../_services/animals/animal/animal-servi
   })
   export class AnimalListComponent implements OnInit {
 
-    public animales: any [];
-    constructor(private animalService: AnimalService) {
+    public animalList: Animal [];
+    constructor(private animalService: AnimalService,
+                private imgService: ImagesService) {
 
     }
 
     ngOnInit() {
-        this.animalService.getAnimals().subscribe(animals => {
-          this.animales = animals.response;
+      this.animalService.getAnimals().subscribe(animals => {
+        this.animalList = animals.response;
+        this.animalList.forEach(animal => {
+
+          this.imgService.getImagesByAnimal(animal['id']).subscribe(imgAnimal => {
+            console.log(imgAnimal);
+            animal.pictures = imgAnimal.response[0].image;
+            console.log('animales: ', this.animalList);
+          });
         });
+      });
     }
 
   }
