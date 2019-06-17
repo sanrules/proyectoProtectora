1ee26d3f6f39de269ea4a1747853107483e3f32b
Fernando <fgironpe@gmail.com>
2019-06-17 21:34:25 +0200

    arreglada busqueda por otros animales

--- ProtectoraWeb/src/app/components/web/animals/animal-list/animal-list.component.ts
+++ ProtectoraWeb/src/app/components/web/animals/animal-list/animal-list.component.ts
@@ -3,6 +3,7 @@ import { AnimalService } from '../../../../_services/animals/animal/animal-servi
 import { ImagesService } from '../../../../_services/animals/images/images.service';
 import { ActivatedRoute } from '@angular/router';
 import { ComponentFactoryResolver } from '@angular/core/src/render3';
+import { AnimalTypeService } from 'src/app/_services/animals/tipo-animal/animal-type-service';
 
 
 @Component({
@@ -15,23 +16,37 @@ export class AnimalListComponent implements OnInit {
   /* public animalList: Animal []; */
   public animalList: any[] = [];
   public animals: any[] = [];
-  public animalType: any;
+  public searchType: any;
   public from: string;
 
   constructor(private route: ActivatedRoute,
               private animalService: AnimalService,
+              private typeService: AnimalTypeService,
               private imgService: ImagesService) {
   }
 
   ngOnInit() {
 
-    this.route.params.subscribe(type => {
+    this.route.params.subscribe(param => {
 
-      this.animalType = +type.type;
+      this.searchType = +param.type;
       this.from = 'list';
-      if (this.animalType !== 0) {
-        console.log('from: ', this.from);
-        this.animalService.getAnimalByType(this.animalType).subscribe(animals => {
+      if (this.searchType === 3) {
+        this.animalService.getAnimalByStatus(1).subscribe(animals => {
+          this.animals = animals['response'];
+
+          this.animals.forEach(animal => {
+            if (+animal.type !== 2 && +animal.type !== 1) {
+              this.imgService.getImagesByAnimal(animal['id']).subscribe(imgAnimal => {
+                animal.image = imgAnimal.response[0].image;
+                this.animalList.push(animal);
+              });
+            }
+          });
+          console.log('animalList', this.animalList);
+        });
+       } else if (this.searchType !== 0) {
+        this.animalService.getAnimalByType(this.searchType).subscribe(animals => {
           this.animals = animals['response'];
 
           this.animals.forEach(animal => {
@@ -61,6 +76,19 @@ export class AnimalListComponent implements OnInit {
 
 
   }
+
+  getCats () {
+
+  }
+
+  getDogs() {
+
+  }
+
+  getOtherTypes () {
+
+  }
+
 
 }
 
